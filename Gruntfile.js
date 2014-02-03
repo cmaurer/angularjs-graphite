73680ff1c6525142e5c20de3c02710b756058a5c
Chris Maurer <chris.maurer@wnco.com>
2014-02-03 16:11:47 -0600

    added travis added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+/*global module:false*/
+module.exports = function (grunt) {
+  'use strict';
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= pkg.license %> */\n',
+    // Task configuration.
+    clean: ['dist/'],
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      js: {
+        src: ['src/namespace.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      afterconcat: ['dist/angularjs-graphite.js'],
+    },
+    jsbeautifier : {
+      files : ['dist/angularjs-graphite.js'],
+      options : {
+        js: {
+          evalCode: true,
+          indentSize: 2,
+          indentChar: ' ',
+          spaceInParen: true,
+          jslintHappy: true,
+          indentLevel: 0
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-jsbeautifier');
+
+  // Default task.
+  grunt.registerTask('default', ['clean', 'concat', 'jsbeautifier', 'jshint']);
+
+};
