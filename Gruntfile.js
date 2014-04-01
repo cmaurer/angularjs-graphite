8ee086f544db81ec5931eb602805b3f681abdfc5
Chris Maurer <chris.maurer@wnco.com>
2014-04-01 06:58:45 -0500

    upgraded grunt-contrib-concat to 0.4.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -152,7 +152,7 @@ module.exports = function (grunt) {
     protractor: {
       options: {
         configFile: "node_modules/protractor/referenceConf.js", // Default config file
-        keepAlive: true, // If false, the grunt process stops when the test fails.
+        keepAlive: false, // If false, the grunt process stops when the test fails.
         noColor: false, // If true, protractor will not use colors in its output.
         args: {
           // Arguments passed to the command
