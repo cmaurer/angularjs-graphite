13af15b36557686d7b4d7431eabc23aee702e0f1
Chris Maurer <chris.maurer@wnco.com>
2014-04-08 15:58:13 -0500

    fixing e2e test

--- test/e2e/basic.js
+++ test/e2e/basic.js
@@ -16,7 +16,10 @@ describe('angularjs.grapahite', function () {
       expect(testName.getText()).toEqual('test');
 
       var url = element(by.binding('url'));
-      expect(url.getText()).toEqual('http://localhost:9001/render?format=json&from=-1h&until=-15s&target=foobar.one.five&target=foobar.one.six&target=foobar.one.seven&target=foobar.two.five&target=foobar.two.six&target=foobar.two.seven&target=foobar.three.five&target=foobar.three.six&target=foobar.three.seven');
+      expect(url.getText()).toEqual('http://localhost:9001/render?format=json&from=-1h&until=-15s&target=foobar.one.five&target=foobar.two.five&target=foobar.three.five&target=foobar.one.six&target=foobar.two.six&target=foobar.three.six&target=foobar.one.seven&target=foobar.two.seven&target=foobar.three.seven');
+
+//      http://localhost:9001/render?format=json&from=-1h&until=-15s&target=foobar.one.five&target=foobar.two.five&target=foobar.three.five&target=foobar.one.six&target=foobar.two.six&target=foobar.three.six&target=foobar.one.seven&target=foobar.two.seven&target=foobar.three.seven
+//      http://localhost:9001/render?format=json&from=-1h&until=-15s&target=foobar.one.five&target=foobar.one.six&target=foobar.one.seven&target=foobar.two.five&target=foobar.two.six&target=foobar.two.seven&target=foobar.three.five&target=foobar.three.six&target=foobar.three.seven
 
       var status = element(by.binding('status'));
       expect(status.getText()).toMatch('200');
